5dd51734b292138ddfdb94f7e355d783fd17fd5e
James Jackson <superjax08@gmail.com>
2018-01-06 17:22:16 -0700

    fixed some warning in compiling on pixhawk

--- src/gps_helper.h
+++ src/gps_helper.h
@@ -159,7 +159,7 @@ public:
 	 * @return 0 on success, <0 on error
 	 */
 	virtual int restartSurveyIn() { return 0; }
-	virtual void setSurveyInSpecs(uint32_t survey_in_acc_limit, uint32_t survey_in_min_dur) = 0;
+	void setSurveyInSpecs(uint32_t survey_in_acc_limit, uint32_t survey_in_min_dur){};
 
 
 protected:
